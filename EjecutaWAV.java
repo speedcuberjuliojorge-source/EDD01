7fd5bf205963e18351a7961c6c1e6fb24f62c85e
Julio Cruz <speedcuberjuliojorge@gmail.com>
2026-09-16 20:30:50 -0600

    Primer avance

---
+++ EjecutaWAV.java
@@ -0,0 +1,100 @@
+package generaWav;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+/**
+ * Punto de entrada principal para la aplicación de generación de archivos de
+ * sonido WAV. Su diseño se enfoca en la invocación por consola y la propagación
+ * explícita de errores mediante excepciones.
+ *
+ * @author julio_cubes
+ */
+public class EjecutaWAV {
+
+    /**
+     * Metodo main del programa
+     *
+     * @param args
+     * @throws java.io.IOException
+     * @throws java.lang.IllegalArgumentException
+     */
+    public static void main(String args[]) throws java.io.IOException,
+            java.lang.IllegalArgumentException {
+
+        //DECLARACION DE VARIABLES
+        String archControl = "";
+        BufferedReader lectorArch;
+        String nombreArchAud;
+        int frecuencia_muestreo;
+        int senial_armonica;
+        int duracion;
+        GeneraWAV generaWav;
+
+        if (args.length == 1) {
+            archControl = args[0];
+        } else {
+            throw new java.lang.IllegalArgumentException();
+        }
+
+        /*Verificacion de que se hayan proporcionado
+        el número correcto de argumentos de línea de comandos.*/
+        if (args.length == 1) {
+
+            //Se comprueba si existe el archivo de control
+            File fArchControl = new File(archControl);
+            /*Si si existe el archivo de control, se lee el archivo de control
+            y se guardan los valores*/
+            if (fArchControl.exists()) {
+                lectorArch = new BufferedReader(new FileReader(archControl));
+                System.out.println(">> " + archControl);
+                nombreArchAud = lectorArch.readLine();
+                System.out.println("Nombre arch. de audio: " + nombreArchAud);
+                try {
+                    frecuencia_muestreo = Integer.parseInt(lectorArch.readLine());
+                    System.out.println("Frecuencia de muestreo: " + frecuencia_muestreo);
+                } catch (java.lang.IllegalArgumentException e) {
+                    throw new java.lang.IllegalArgumentException();
+
+                }
+
+                try {
+                    senial_armonica = Integer.parseInt(lectorArch.readLine());
+                    System.out.println("Señal armonica: " + senial_armonica);
+                } catch (java.lang.IllegalArgumentException e) {
+                    throw new java.lang.IllegalArgumentException();
+                }
+
+                try {
+                    duracion = Integer.parseInt(lectorArch.readLine());
+                    System.out.println("Duracion de audio: " + duracion);
+                } catch (java.lang.IllegalArgumentException e) {
+                    throw new java.lang.IllegalArgumentException();
+                }
+                lectorArch.close();
+                //Si no existe el archivo de control, se tira una exepcion
+            } else {
+                throw new java.io.FileNotFoundException(/*"No se encontro el archivo de control"*/);
+            }
+
+            /*En caso de recibir más parametros de los esperados,
+            se tira una exepcion*/
+        } else {
+            throw new java.lang.IllegalArgumentException(/*"Numero de paramentros args[] incorrecto"*/);
+        }
+
+        //invocar a la clase generaWav.GeneraWAV para realizar la creación efectiva del archivo WAV.
+        generaWav = new GeneraWAV();
+        generaWav.escribe(nombreArchAud, duracion, frecuencia_muestreo, duracion);
+        /*
+        EXEPCIONES QUE PUEDE LANZAR EL PROGRAMA
+        java.lang.IllegalArgumentException:Si el número de argumentos de línea de comandos es distinto de uno.
+        NumberFormatException: Si un valor en el archivo de configuración no puede ser parseado a un entero válido
+        java.io.FileNotFoundException: Si el archivo de configuración especificado por el argumento de línea de comandos no existe.
+        java.io.IOException: Si ocurre cualquier problema durante la lectura del archivo de configuración (ej. archivo incompleto, problemas de permisos, fin de archivo inesperado).
+        Cualquier excepción (ej. java.lang.IllegalArgumentException, java.lang.NullPointerException) que sea lanzada por la clase generaWav.GeneraWAV debido a validaciones de los parámetros de generación del WAV.
+         */
+    }
+
+}
---
+++ GeneraWAV.java
@@ -0,0 +1,40 @@
+package generaWav;
+
+public class GeneraWAV {
+
+    /**
+     * realizar la creación efectiva del archivo WAV y validar los valores
+     */
+    public void GeneraWAV() {
+
+    }
+
+    /**
+     * La funcionalidad de la API se expone a través del método escribe. Crea el
+     * archivo de audio de onda sinuseidal. Realizar validaciones de 2.
+     * Comportamiento y Validaciones
+     */
+    public void escribe(String nombre, int iTiempo,
+            int iFrecuenciaMuestreo, int iArmonico) {
+        if (nombre == null) {
+            throw new java.lang.NullPointerException();
+        }
+        if (iTiempo < 0 || iFrecuenciaMuestreo < 0 || iArmonico < 0) {
+            throw new java.lang.IllegalArgumentException();
+        }
+        if (iArmonico > 20000 || iArmonico < 15430) {
+            throw new java.lang.IllegalArgumentException();
+        }
+        /*
+        java.lang.NullPointerException: Se lanza si el parámetro nombre es nulo.
+        java.lang.IllegalArgumentException: Se lanza bajo las siguientes condiciones:
+            o Si los valores de iTiempo, iFrecuenciaMuestreo o armonico son menor que 0.
+            o Si el valor de armonico es superior a 20000 Hz, lo cual excede el límite de
+            escucha humana. Tambien si el armonico es inferior a 15430
+            o Si el parámetro name es una cadena vacía, solo contiene espacios en blanco, o no
+            tiene un nombre válido y la extensión .wav.
+         */
+
+    }
+
+}
